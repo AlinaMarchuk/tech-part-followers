6d56debf7c5fa51b4679a3066374ef915d67acdc
AlinaMarchuk <123773400+AlinaMarchuk@users.noreply.github.com>
2023-06-05 16:58:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/tech-part-followers">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
